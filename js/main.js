441f67853f5a71218853e56993dc5274472d622f
Angus Macrae <angusdmacrae@gmail.com>
2021-04-27 22:40:40 +0100

    Lay out component structure

--- js/main.js
+++ js/main.js
@@ -1,5 +1,51 @@
+function Header() {
+  return <header></header>;
+}
+
+function Footer() {
+  return <footer></footer>;
+}
+
+function SearchableNewsFeed() {
+  return (
+    <div>
+      <SearchableNewsFeedInput />
+      <SearchableNewsFeedResults />
+    </div>
+  );
+}
+
+function SearchableNewsFeedInput() {
+  return <div></div>;
+}
+
+function SearchableNewsFeedResults() {
+  return (
+    <ul>
+      <SearchableNewsFeedItem />
+      <SearchableNewsFeedItem />
+    </ul>
+  );
+}
+
+function SearchableNewsFeedItem() {
+  return (
+    <li>
+      <article></article>
+    </li>
+  );
+}
+
 function App() {
-  return <div>App goes here</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <SearchableNewsFeed />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
